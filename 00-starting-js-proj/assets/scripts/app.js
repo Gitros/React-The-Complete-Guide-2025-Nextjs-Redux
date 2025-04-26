0625541d6fb15deeecd67f1ea3ce0ea61650c7d2
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-04-26 15:38:51 +0200

    objects and classes

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -6,9 +6,34 @@
 
 // console.log(util.apiKey);
 
-function greetUser(userName, message) {
-  return "Hi i am " + userName + "." + message;
+// function greetUser(userName, message) {
+//   return "Hi i am " + userName + "." + message;
+// }
+
+// greetUser("Max", "Hello!");
+// greetUser("Max", "Hello test!");
+
+const user = {
+  name: "Max",
+  age: 34,
+  greet() {
+    console.log("Hello!");
+  },
+};
+
+console.log(user.name);
+user.greet();
+
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log("Hi!");
+  }
 }
 
-greetUser("Max", "Hello!");
-greetUser("Max", "Hello test!");
+const user1 = new User("Manuel", 35);
+console.log(user1);
