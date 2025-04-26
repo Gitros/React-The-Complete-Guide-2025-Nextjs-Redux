d5c8134e1663dcfcd0acb9b7ee9a38142fef7ab5
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-04-26 15:35:16 +0200

    function

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -2,6 +2,13 @@
 
 // import apiKey from "./util.js";
 
-import * as util from "./util.js";
+// import * as util from "./util.js";
 
-console.log(util.apiKey);
+// console.log(util.apiKey);
+
+function greetUser(userName, message) {
+  return "Hi i am " + userName + "." + message;
+}
+
+greetUser("Max", "Hello!");
+greetUser("Max", "Hello test!");
